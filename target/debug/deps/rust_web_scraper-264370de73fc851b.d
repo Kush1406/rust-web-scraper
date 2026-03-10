7de003529b950a648dd348be7dfaab3c40d669f5
Elliot <kushagra1499@gmail.com>
2026-03-10 07:41:50 +0530

    first commit

---
+++ target/debug/deps/rust_web_scraper-264370de73fc851b.d
@@ -0,0 +1,5 @@
+/home/elliot/rust_claude/rust-web-scraper/target/debug/deps/rust_web_scraper-264370de73fc851b.d: src/main.rs
+
+/home/elliot/rust_claude/rust-web-scraper/target/debug/deps/librust_web_scraper-264370de73fc851b.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/rust_web_scraper-fdf7ed4a23a79579.d
@@ -0,0 +1,5 @@
+/home/elliot/rust_claude/rust-web-scraper/target/debug/deps/rust_web_scraper-fdf7ed4a23a79579.d: src/main.rs
+
+/home/elliot/rust_claude/rust-web-scraper/target/debug/deps/librust_web_scraper-fdf7ed4a23a79579.rmeta: src/main.rs
+
+src/main.rs:
